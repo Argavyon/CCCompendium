d6e6fa7b724c1d11aaeeef49827540261a1023c3
Argavyon <75849479+Argavyon@users.noreply.github.com>
2022-11-05 20:09:20 +0100

    Properly sort spells

--- .github/workflows/createDB.py
+++ .github/workflows/createDB.py
@@ -5,5 +5,6 @@ with open('data/SpellDB.js', 'w') as DBfile:
     for i in glob.iglob('data/spells/*.json'):
         with open(i, 'r') as spellJSON:
             spell_list.append(json.load(spellJSON))
-    spellDB = json.dumps(spell_list, sort_keys=True, indent=4)
+    spell_list.sort(key = lambda spell: spell['tier'])
+    spellDB = json.dumps(spell_list, indent=2)
     print('const spellDatabase = ', spellDB, ';', sep = '', file = DBfile)
