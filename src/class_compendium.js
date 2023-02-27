007222b13dfaf06114213d5c12d7f0466c02632c
Argavyon <75849479+Argavyon@users.noreply.github.com>
2023-02-27 19:31:09 +0100

    Work some on the class compendium

--- src/class_compendium.js
+++ src/class_compendium.js
@@ -47,11 +47,18 @@ function featureCard(featureData) {
     const card = document.createElement('table');
 	const cardBody = card.appendChild(document.createElement('tbody'));
 	card.className = 'class';
-    
+
     const featureName = cardBody.appendChild(document.createElement('tr')).appendChild(document.createElement('td'));
 	featureName.className = 'classname';
-	featureName.colSpan = 2;
-	featureName.textContent = featureData.name;
+    featureName.textContent = featureData.name;
+    
+    const level = cardBody.appendChild(document.createElement('tr')).appendChild(document.createElement('td'));
+	level.className = 'classtype';
+	level.textContent = `Level ${featureData.level}`;
+    
+    const desc = cardBody.appendChild(document.createElement('tr')).appendChild(document.createElement('td'));
+    desc.className = 'classdata';
+	desc.innerHTML = featureData.desc;
     
     return card;
 }
@@ -59,7 +66,29 @@ function featureCard(featureData) {
 function talentCard(talentData) {
     const card = document.createElement('table');
 	const cardBody = card.appendChild(document.createElement('tbody'));
-	card.className = 'class';
+    card.className = 'class';
+
+    const talentName = cardBody.appendChild(document.createElement('tr')).appendChild(document.createElement('td'));
+    talentName.className = 'classname';
+    talentName.colSpan = 2;
+	talentName.textContent = talentData.name;
+
+    if (talentData.prereq) {
+        const prereqRow = cardBody.appendChild(document.createElement('tr'));
+
+        const prereqL = prereqRow.appendChild(document.createElement('td'));
+        prereqL.className = 'classname';
+        prereqL.textContent = 'Prerequisites:';
+
+        const prereqR = prereqRow.appendChild(document.createElement('td'));
+        prereqR.className = 'classdata';
+        prereqR.textContent = talentData.prereq.join(', ');
+    }
+    
+    const desc = cardBody.appendChild(document.createElement('tr')).appendChild(document.createElement('td'));
+    desc.className = 'classdata';
+    desc.colSpan = 2;
+	desc.innerHTML = talentData.desc;
     
     return card;
 }
@@ -67,7 +96,25 @@ function talentCard(talentData) {
 function weaponStyleCard(styleData) {
     const card = document.createElement('table');
 	const cardBody = card.appendChild(document.createElement('tbody'));
-	card.className = 'class';
+    card.className = 'class';
+    
+    const styleName = cardBody.appendChild(document.createElement('tr')).appendChild(document.createElement('td'));
+	styleName.className = 'classname';
+    styleName.colSpan = 2;
+    styleName.textContent = styleData.name;
+
+    Object.entries(styleData).filter(([attack, _]) => attack != 'name').forEach(([attack, desc]) => {
+        const attackRow = cardBody.appendChild(document.createElement('tr'));
+
+        const attackName = attackRow.appendChild(document.createElement('td'));
+        attackName.className = 'classname';
+        attackName.textContent = attack;
+        attackName.style.fontStyle = "italic";
+
+        const attackDesc = attackRow.appendChild(document.createElement('td'));
+        attackDesc.className = 'classdata';
+        attackDesc.innerHTML = desc;
+    });
     
     return card;
 }
@@ -75,7 +122,15 @@ function weaponStyleCard(styleData) {
 function combatStyleCard(styleData) {
     const card = document.createElement('table');
 	const cardBody = card.appendChild(document.createElement('tbody'));
-	card.className = 'class';
+    card.className = 'class';
+    
+    const styleName = cardBody.appendChild(document.createElement('tr')).appendChild(document.createElement('td'));
+    styleName.className = 'classname';
+    styleName.textContent = styleData.name;
+    
+    const desc = cardBody.appendChild(document.createElement('tr')).appendChild(document.createElement('td'));
+    desc.className = 'classdata';
+	desc.innerHTML = styleData.desc;
     
     return card;
 }
@@ -83,13 +138,22 @@ function combatStyleCard(styleData) {
 function maneuverCard(maneuverData) {
     const card = document.createElement('table');
 	const cardBody = card.appendChild(document.createElement('tbody'));
-	card.className = 'class';
+    card.className = 'class';
+    
+    const maneuverName = cardBody.appendChild(document.createElement('tr')).appendChild(document.createElement('td'));
+	maneuverName.className = 'classname';
+    maneuverName.textContent = maneuverData.name;
+    
+    const desc = cardBody.appendChild(document.createElement('tr')).appendChild(document.createElement('td'));
+    desc.className = 'classdata';
+    desc.colSpan = 2;
+	desc.innerHTML = maneuverData.desc;
     
     return card;
 }
 
 const Dragonlord = {
-    'class': {
+    class: {
         name: 'Dragonlord',
         type: 'Half-Caster',
         dice: '10',
@@ -118,17 +182,20 @@ const Dragonlord = {
 };
 const Talents = {
     'Dragon Breath': {
-        'class': ['Dragonlord'],
+        name: 'Dragon Breath',
+        class: ['Dragonlord'],
         desc: 'You can spend 1 AP to make a breath attack. When you take this talent, choose the damage type of the breath attack, which cannot be changed. Your breath targets either a 10 foot radius around you or a 30 foot line. All targets must succeed on a Dexterity saving throw or take 1d6 damage. This damage increases by 1d6 at 10th and 20th level.',
     },
-    'Mind and Soul' :{
-        'class': ['Dragonlord'],
+    'Mind and Soul': {
+        name: 'Mind and Soul',
+        class: ['Dragonlord'],
         prereq: ['In Tune', 'Elder Dragon\'s Knowledge', 'Speaker of the Wyrmtongue'],
         desc: 'Dragonlord becomes a full caster, increasing its MP to 12/level. Draconic spells you cast grant you temporary hit points equal to two times their tier until the start of your next turn. You are immune to the frightened condition while you are affected by a Draconic spell.',
     },
 };
 const WeaponStyles = {
     Dagger: {
+        name: 'Dagger',
         'Quick Stab': 'Aims for an exposed part and stabs. Deals normal weapon damage. The weapon damage roll is doubled against a creature that can\'t move or with a movement speed of 0.',
         Disembark: 'A careful thrust meant to secure an opening for you. Deals half weapon damage, and the target can\'t make attacks of opportunity against you even if you miss.',
         Combo: 'A lethal strike that requires you to have struck your enemy already this turn with a different weapon other than another dagger. Damage die increased by 1d4.',
@@ -136,11 +203,13 @@ const WeaponStyles = {
 };
 const CombatStyles = {
     Skirmisher: {
+        name: 'Skirmisher',
         desc: 'The first time you hit a creature with a weapon attack on your turn, you gain an additional 10 feet of movement for the rest of the turn. The bonus increases by 5 feet at 5th, 10th, and 15th level.',
     },
 };
 const Maneuvers = {
     'Quick Draw': {
+        name: 'Quick Draw',
         class: ['Warrior#3'],
         desc: 'You immediately draw a new weapon and attack with that weapon. On a successful hit, double the damage bonus from Strength or Dexterity.',
     },
@@ -150,15 +219,15 @@ const content = document.querySelector('#class_compendium_content');
 
 content.appendChild(classCard(Dragonlord.class));
 
-content.appendChild(featureCard(Dragonlord.features.Spellcasting));
+content.appendChild(featureCard(Dragonlord.features['Spellcasting']));
 content.appendChild(featureCard(Dragonlord.features['Draconic Rage']));
 content.appendChild(featureCard(Dragonlord.features['Ambient Power']));
 
 content.appendChild(talentCard(Talents['Dragon Breath']));
 content.appendChild(talentCard(Talents['Mind and Soul']));
 
-content.appendChild(weaponStyleCard(WeaponStyles.Dagger));
+content.appendChild(weaponStyleCard(WeaponStyles['Dagger']));
 
-content.appendChild(combatStyleCard(CombatStyles.Skirmisher));
+content.appendChild(combatStyleCard(CombatStyles['Skirmisher']));
 
 content.appendChild(maneuverCard(Maneuvers['Quick Draw']));
