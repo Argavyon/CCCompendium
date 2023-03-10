5531281db62e8ec858b3c0eb58e959b198ab8e95
Argavyon <75849479+Argavyon@users.noreply.github.com>
2023-03-10 14:19:58 +0000

    Auto-generated data/TalentDB.js

---
+++ data/TalentDB.js
@@ -0,0 +1,19 @@
+const talentDatabase = {
+  "Mind and Soul.json": {
+    "class": [
+      "Dragonlord"
+    ],
+    "prereq": [
+      "In Tune",
+      "Elder Dragon's Knowledge",
+      "Speaker of the Wyrmtongue"
+    ],
+    "desc": "Dragonlord becomes a full caster, increasing its MP to 12/level. Draconic spells you cast grant you temporary hit points equal to two times their tier until the start of your next turn. You are immune to the frightened condition while you are affected by a Draconic spell."
+  },
+  "Dragon Breath.json": {
+    "class": [
+      "Dragonlord"
+    ],
+    "desc": "You can spend 1 AP to make a breath attack. When you take this talent, choose the damage type of the breath attack, which cannot be changed. Your breath targets either a 10 foot radius around you or a 30 foot line. All targets must succeed on a Dexterity saving throw or take 1d6 damage. This damage increases by 1d6 at 10th and 20th level."
+  }
+};
