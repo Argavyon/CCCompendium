0a7f7a70536839b480c63e38e46a5148301fc477
Argavyon <75849479+Argavyon@users.noreply.github.com>
2023-03-10 16:14:32 +0100

    Remove old DBs

--- data/CombatStyleDB.js
+++
@@ -1,5 +0,0 @@
-const combatStyleDatabase = {
-  "Skirmisher.json": {
-    "desc": "The first time you hit a creature with a weapon attack on your turn, you gain an additional 10 feet of movement for the rest of the turn. The bonus increases by 5 feet at 5th, 10th, and 15th level."
-  }
-};
--- data/WeaponStyleDB.js
+++
@@ -1,7 +0,0 @@
-const weaponStyleDatabase = {
-  "Dagger.json": {
-    "Quick Stab": "Aims for an exposed part and stabs. Deals normal weapon damage. The weapon damage roll is doubled against a creature that can't move or with a movement speed of 0.",
-    "Disembark": "A careful thrust meant to secure an opening for you. Deals half weapon damage, and the target can't make attacks of opportunity against you even if you miss.",
-    "Combo": "A lethal strike that requires you to have struck your enemy already this turn with a different weapon other than another dagger. Damage die increased by 1d4."
-  }
-};
