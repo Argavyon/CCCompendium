81931721ce845141e88bc081a8559965f78ebe94
Argavyon <75849479+Argavyon@users.noreply.github.com>
2024-02-03 22:04:47 +0000

    Auto-generated DBs

--- data/ManeuverDB.js
+++ data/ManeuverDB.js
@@ -1,8 +1,9 @@
-const ManeuverDatabase = {
-  "Quick Draw": {
+const ManeuverDatabase = [
+  {
+    "name": "Quck Draw",
     "class": [
       "Warrior#3"
     ],
     "desc": "You immediately draw a new weapon and attack with that weapon. On a successful hit, double the damage bonus from Strength or Dexterity."
   }
-};
+];
--- data/WeaponStyleDB.js
+++ data/WeaponStyleDB.js
@@ -1,7 +1,19 @@
-const WeaponStyleDatabase = {
-  "Dagger": {
-    "Quick Stab": "Aims for an exposed part and stabs. Deals normal weapon damage. The weapon damage roll is doubled against a creature that can't move or with a movement speed of 0.",
-    "Disembark": "A careful thrust meant to secure an opening for you. Deals half weapon damage, and the target can't make attacks of opportunity against you even if you miss.",
-    "Combo": "A lethal strike that requires you to have struck your enemy already this turn with a different weapon other than another dagger. Damage die increased by 1d4."
+const WeaponStyleDatabase = [
+  {
+    "name": "Dagger",
+    "attacks": [
+      {
+        "name": "Quick Stab",
+        "desc": "Aims for an exposed part and stabs. Deals normal weapon damage. The weapon damage roll is doubled against a creature that can't move or with a movement speed of 0."
+      },
+      {
+        "name": "Disembark",
+        "desc": "A careful thrust meant to secure an opening for you. Deals half weapon damage, and the target can't make attacks of opportunity against you even if you miss."
+      },
+      {
+        "name": "Combo",
+        "desc": "A lethal strike that requires you to have struck your enemy already this turn with a different weapon other than another dagger. Damage die increased by 1d4."
+      }
+    ]
   }
-};
+];
